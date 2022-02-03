cc4c0b47468cd3ae7e3f9195f4b03471ac9185cb
Thiana19 <55779794+Thiana19@users.noreply.github.com>
2022-02-03 21:00:30 +0800

    Update Landing.dart

--- lib/Landing.dart
+++ lib/Landing.dart
@@ -10,26 +10,60 @@ class _LandingState extends State<Landing> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-     body: Container(
-          constraints: BoxConstraints.expand(),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("./assets/cloud.jpg"), 
-                fit: BoxFit.cover),
-          ),
+     body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget> [
           
-          child: 
-            Padding(
-              padding: const EdgeInsets.only(top: 32, bottom: 10),
-              child: const Text('MALAYSIAN WEATHER UPDATE',
+          Container(
+            color: Colors.cyan,
+            child: Padding(
+              padding: const EdgeInsets.only(top:10, bottom:10),
+              child: Text("MALAYSIAN WEATHER UPADATE",
               textAlign: TextAlign.center,
               style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.white),
               ),
             ),
-
+          ),
           
+          Container(
+              child: Padding( 
+              padding: const EdgeInsets.only(top:10, bottom:10),
+              child: Image.asset('./assets/icons.png',
+              width: 600,
+              height: 150,
+              ),
+            ),
+          ),
+          
+          Container(
+            margin: EdgeInsets.all(25),  
+              child: FlatButton(  
+                child: Text('Register', style: TextStyle(fontSize: 20.0),),  
+                color: Colors.cyan,  
+                textColor: Colors.white,  
+                onPressed: () {},  
+              ),  
+          ),
 
-          )
-        );
+          Container(
+            child: Text("Or",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          
+          Container(
+            margin: EdgeInsets.all(20),  
+              child: FlatButton(  
+                child: Text('Continue as a guest', style: TextStyle(fontSize: 20.0),),  
+                color: Colors.cyan,  
+                textColor: Colors.white,  
+                onPressed: () {},  
+              ),  
+          ),
+        ],
+      )
+    );
   }
 }
